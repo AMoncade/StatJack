8b8fe7612c7055a716db32b6ed5f71e9622e8b4a
AMoncade <adrien@moncade.com>
2026-02-11 08:56:19 -0500

    logique du programme architecturer (faut faire le code mais au moins les choses sont relier et creer)

---
+++ src/models/jeu.py
@@ -0,0 +1,9 @@
+from src.models.sabot import Sabot
+from src.models.main_joueur import MainJoueur
+
+#Doit gerer le déroulement d'une manche
+#Penser que joueur peut split, double, hit, stand apres avoir recu ses deux cartes
+#Penser dealer doit tirer jusqu'a ce qu'il atteigne au minimum 17
+
+class Jeu:
+    pass
--- src/models/main_joueur.py
+++ src/models/main_joueur.py
@@ -1,3 +1,6 @@
 from src.models.carte import Carte
 
 #Donner une main aleatoirement au joueur (s'assurrer que A = 11 puisse devenir A = 1 (regle bj))
+
+class MainJoueur:
+    pass
