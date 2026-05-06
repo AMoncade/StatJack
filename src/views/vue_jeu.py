78bf911a8637e181e1dd3855ed752b4dbf17211f
AMoncade <adrien@moncade.com>
2026-05-06 15:23:25 -0400

    bypass forcer a jouer mode entrainemnet

--- src/views/vue_jeu.py
+++ src/views/vue_jeu.py
@@ -855,8 +855,9 @@ class VueJeu(QWidget):
         msg_box.setInformativeText("Voulez-vous corriger votre action ou continuer malgré tout ?")
 
         btn_corriger = msg_box.addButton("Corriger", QMessageBox.AcceptRole)
-        btn_continuer = msg_box.addButton("Continuer quand même", QMessageBox.RejectRole)
+        btn_continuer = msg_box.addButton("Continuer quand même", QMessageBox.ActionRole)
         msg_box.setDefaultButton(btn_corriger)
+        msg_box.setEscapeButton(btn_corriger)
 
         msg_box.setStyleSheet("""
             QMessageBox { background-color: #1a1a2e; color: white; }
